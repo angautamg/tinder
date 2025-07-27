d18d3736c6bc39b9031f4204cb1b69bfe9af5dcb
Gautam singh <angautamg@gmail.com>
2025-07-27 18:08:23 +0530

    Modify api in app js for test

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ app.use('/', (req, res) => {
   res.send('Welcome to Javascript')
 })
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
